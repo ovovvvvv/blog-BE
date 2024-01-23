c08c5cbf1e8baf1e67a48730ad58679308911ff6
ovovvvvv <ovovvvvv@naver.com>
2024-01-24 00:19:53 +0900

    페이지네이션 구현

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,28 +1,77 @@
 import Post from '../../models/post.js';
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+    const {id} = ctx.params;
+    if (!ObjectId.isValid(id)){
+        ctx.status = 400;
+        return;
+    }
+    return next();
+};
 
 // 포스트 작성
 export const write = async ctx  => {
-    const { title, body, tags} = ctx.request.body;
+    const schema = Joi.object().keys({
+        // 객체가 다음 필드를 가지고 있음을 검증해줌
+        title : Joi.string().required(), // required가 있으면 필수항목
+        body: Joi.string().required(),
+        tags: Joi.array()
+            .items(Joi.string())
+            .required(), // 문자열로 이루어진 배열
+    });
+
+    // 검증하고 나서 검증 실패인 경우 에러 처리
+    const result = schema.validate(ctx.request.body);
+    if(result.error) {
+        ctx.status = 400;  // Bad request
+        ctx.body = result.error;
+        return;
+    }
+    const { title, body, tags } = ctx.request.body;
     const post = new Post({
         title, 
         body,
         tags,
     });
-    try {
+
+    try{
         await post.save();
         ctx.body = post;
     } catch (e) {
-        ctx.throw(500,e);
+        ctx.throw(500, e);
     }
 };
 
 // 포스트 전체 조회
 export const list = async ctx => {
+    const page = parseInt(ctx.query.page || '1', 10);
+
+    if (page < 1) {
+        ctx.status = 400;
+        return;
+    }
+
     try {
-        const posts = await Post.find().exec();
-        ctx.body = posts;
-    } catch (e) {
-        ctx.throw(500, e);
+        const posts = await Post.find()
+        .sort({_id: -1})
+        .limit(10)
+        .skip((page - 1) * 10)
+        .lean()
+        .exec();
+        const postCount = await Post.countDocuments().exec();
+        // countDocuments =  MongoDB에서 Post 모델의 문서 수를 가져오는 Mongoose 메서드
+        ctx.set('Last-Page', Math.ceil(postCount / 10));
+        ctx.body = posts.map(post =>({
+                ...post,
+                body:
+                    post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+            }));
+    } catch (err) {
+        ctx.throw(500, err);
     }
 };
 
@@ -53,6 +102,22 @@ export const remove = async ctx => {
 
 export const update = async ctx => {
     const {id} = ctx.params;
+
+    const schema = Joi.object().keys({
+        // 객체가 다음 필드를 가지고 있음을 검증해줌
+        title : Joi.string(), // required가 있으면 필수항목
+        body: Joi.string(),
+        tags: Joi.array().items(Joi.string()),
+    });
+
+    // 검증 후 실패인 경우 에러 처리
+    const result = schema.validate(ctx.request.body);
+    if(result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
+
     try {
         const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
             new : true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
