c348055d33fc7e240e77f2f516b0023db0da37f1
ovovvvvv <ovovvvvv@naver.com>
2024-01-27 00:33:22 +0900

    jwtMiddleware 만들어서 토큰 검증하기

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -11,6 +11,7 @@ export const register = async ctx => {
             .required(),
         password: Joi.string().required(),
     });
+    // 유효성 검사
     const result = schema.validate(ctx.request.body);
     if(result.error) {
         ctx.status = 400;
@@ -35,17 +36,61 @@ export const register = async ctx => {
 
         // 응답할 데이터에서 hashedPassword 필드 제거
         ctx.body = user.serialize();
+        
+        const token = user.generateToken();
+        ctx.cookies.set('access_token', token, {
+            maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+            httpOnly: true,
+        });
     } catch (e) {
         ctx.throw(500, e);
     }
 };
 
 export const login = async ctx => {
-    // 로그인
-};
+    const { username, password } = ctx.request.body;
 
+    // username, password가 없으면 에러 처리
+    if (!username || !password){
+        ctx.status = 401; // Unauthorized
+        return;
+    };
+
+    try {
+        const user = await User.findByUsername(username);
+        console.log(user);
+        // 계정이 존재하지 않으면 에러 처리
+        if(!user){
+            ctx.status = 401;
+            return;
+        }
+        const valid = await user.checkPassword(password);
+        console.log(valid);
+        // 잘못된 비밀번호
+        if (!valid) {
+            ctx.status = 401;
+            return;
+        }
+        ctx.body = user.serialize();
+            const token = user.generateToken();
+            ctx.cookies.set('access_token', token, {
+                maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+                httpOnly: true,
+            });
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+}
+
+  // 로그인 상태 확인
 export const check = async ctx => {
-    // 로그인 상태 확인
+    const { user } = ctx.state;
+    if (!user) {
+        // 로그인 중 아님
+        ctx.status = 401; // Unauthorized
+        return;
+    }
+    ctx.body = user;
 };
 
 export const logout = async ctx => {
